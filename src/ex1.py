a279922deeb544facb3fc1ae79d423771f8674e2
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-08-15 13:21:12 +0200

    Clean python files
    	-	the volumetric.py file generates in a gui the volumetric
    		visualisation of the last frame
    	-	the clip.py file generates a gif composed of the
    		smoothed surfaces from each saved iteration

--- src/ex1.py
+++
@@ -1,6 +0,0 @@
-import pyvista as pv
-grid = pv.ImageData(dimensions=(9, 9, 9))
-grid['scalars'] = -grid.x
-pl = pv.Plotter()
-_ = pl.add_volume(grid, opacity='linear')
-pl.show()
--- src/hdf5_test.py
+++
@@ -1,4 +0,0 @@
-import h5py
-
-f = h5py.File("data.h5")
-
